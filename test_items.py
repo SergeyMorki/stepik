726470d3dd9bc21a67276afa2be7299b17316651
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2022-07-07 21:45:50 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_button_is(browser):
     browser.get(link)
     assert browser.find_element_by_css_selector('button.btn-add-to-basket'), 'No button'
     time.sleep(30)
